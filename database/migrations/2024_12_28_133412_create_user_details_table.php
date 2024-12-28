a93313946fb21c0e1b7926ce28158e7ffd1a17b7
Thoriq Al Hakim <85446784+thoriqalhakim28@users.noreply.github.com>
2024-12-28 20:44:41 +0700

    feat(database): create migrations for database setup

---
+++ database/migrations/2024_12_28_133412_create_user_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('birth_place')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->enum('gender', ['laki-laki', 'perempuan'])->nullable();
+            $table->boolean('is_married')->default(false)->nullable();
+            $table->string('last_education')->nullable();
+            $table->string('major')->nullable();
+            $table->string('job')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
