a3c04976c70c33dd3d5a11ae199033678161851e
Thoriq Al Hakim <85446784+thoriqalhakim28@users.noreply.github.com>
2025-01-02 19:20:49 +0700

    feat(program): design program detail page for client and create logic for follow the program

--- app/Livewire/User/Programs/ShowProgram.php
+++ app/Livewire/User/Programs/ShowProgram.php
@@ -2,12 +2,41 @@
 
 namespace App\Livewire\User\Programs;
 
+use App\Models\Program;
+use App\Models\UserProgram;
+use Barryvdh\Debugbar\Facades\Debugbar;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 #[Layout('layouts.user')]
 class ShowProgram extends Component
 {
+    public $program;
+
+    public bool $isFollowed = false;
+
+    public function mount($id)
+    {
+        $this->program = Program::with('users')->findOrFail($id);
+
+        $userId = Auth::user()->id;
+
+        $this->isFollowed = $this->program->users->contains($userId);
+    }
+
+    public function follow()
+    {
+        $userId = Auth::user()->id;
+
+        UserProgram::create([
+            'user_id' => $userId,
+            'program_id' => $this->program->id
+        ]);
+
+        $this->isFollowed = true;
+    }
+
     public function render()
     {
         return view('livewire.user.programs.show-program');
