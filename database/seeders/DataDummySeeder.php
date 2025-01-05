0a7748a754845c110ebb1dca79ef948ace5cf67b
Thoriq Al Hakim <85446784+thoriqalhakim28@users.noreply.github.com>
2025-01-05 11:39:25 +0700

    feat(database): create data dummy

---
+++ database/seeders/DataDummySeeder.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Investment;
+use App\Models\Program;
+use App\Models\Transaction;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DataDummySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        try {
+            DB::beginTransaction();
+
+            User::factory(50)->create();
+            Program::factory(20)->create();
+            Investment::factory(20)->create();
+
+            $users = User::all();
+
+            foreach ($users as $user) {
+                $randomPrograms = Program::inRandomOrder()->take(fake()->numberBetween(1, 5))->get();
+
+                $user->programs()->attach($randomPrograms->pluck('id')->toArray(), [
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+
+                foreach ($randomPrograms as $program) {
+                    Transaction::create([
+                        'user_id' => $user->id,
+                        'transactionable_id' => $program->id,
+                        'transactionable_type' => Program::class,
+                        'transaction_date' => now(),
+                        'transaction_type' => fake()->randomElement(['loyalty', 'personal']),
+                        'amount' => fake()->randomFloat(2, 10000, 1000000),
+                        'payment_method' => fake()->randomElement(['bank_transfer', 'credit_card', 'e-wallet']),
+                    ]);
+                }
+
+                $randomInvestments = Investment::inRandomOrder()
+                    ->take(fake()->numberBetween(1, 3))
+                    ->get();
+
+                $user->investments()->attach($randomInvestments->pluck('id')->toArray(), [
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+
+                foreach ($randomInvestments as $investment) {
+                    Transaction::create([
+                        'user_id' => $user->id,
+                        'transactionable_id' => $investment->id,
+                        'transactionable_type' => Investment::class,
+                        'transaction_date' => now(),
+                        'amount' => fake()->randomFloat(2, 10000, 1000000),
+                        'payment_method' => fake()->randomElement(['bank_transfer', 'credit_card', 'e-wallet']),
+                    ]);
+                }
+            }
+            DB::commit();
+        } catch (\Throwable $th) {
+            throw $th;
+
+            DB::rollBack();
+        }
+    }
+}
