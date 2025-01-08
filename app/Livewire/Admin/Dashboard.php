d21eb15f4576d9cdb140df4519ea47665ec09620
Thoriq Al Hakim <85446784+thoriqalhakim28@users.noreply.github.com>
2025-01-08 20:47:44 +0700

    feat(admin): logic for display informations

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -2,14 +2,84 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Investment;
+use App\Models\Program;
+use App\Models\Transaction;
+use App\Models\User;
+use Barryvdh\Debugbar\Facades\Debugbar;
+use Carbon\Carbon;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 #[Layout('layouts.admin')]
 class Dashboard extends Component
 {
+    public $period = 'month';
+
+    #[Computed()]
+    public function programCount()
+    {
+        return Program::count();
+    }
+
+    #[Computed()]
+    public function investmentCount()
+    {
+        return Investment::count();
+    }
+
+    #[Computed()]
+    public function userCount()
+    {
+        return User::role('user')->count();
+    }
+
+    #[Computed()]
+    public function totalTransactions()
+    {
+        $date = Carbon::now();
+
+        switch ($this->period) {
+            case 'year':
+                return Transaction::whereYear('transaction_date', date('Y'))
+                    ->orderBy('transaction_date', 'desc')
+                    ->count();
+                break;
+            case 'month':
+                return Transaction::whereMonth('transaction_date', date('m'))
+                    ->whereYear('transaction_date', date('Y'))
+                    ->orderBy('transaction_date', 'desc')
+                    ->count();
+                break;
+            case 'week':
+                return Transaction::whereBetween('transaction_date', [$date->startOfWeek(), $date->endOfWeek()])
+                    ->orderBy('transaction_date', 'desc')
+                    ->count();
+                break;
+        }
+    }
+
+    #[Computed()]
+    public function recentTransactions()
+    {
+        return Transaction::with(['transactionable', 'user'])
+            ->orderBy('transaction_date', 'desc')
+            ->limit(5)
+            ->get();
+    }
+
     public function render()
     {
-        return view('livewire.admin.dashboard');
+
+        Debugbar::info($this->totalTransactions);
+
+        return view('livewire.admin.dashboard', [
+            'programs' => $this->programCount,
+            'investments' => $this->investmentCount,
+            'users' => $this->userCount,
+            'transactions' => $this->totalTransactions,
+            'recentTransactions' => $this->recentTransactions
+        ]);
     }
 }
--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -9,16 +9,16 @@
                 <div class="flex items-start justify-between">
                     <x-icons.transaction class="w-10 h-10 p-2 text-blue-600 rounded-full bg-blue-100/50" />
                     <div class="w-1/2">
-                        <x-select class="text-xs">
-                            <option value="weekly">Minggu ini</option>
-                            <option value="monthly">Bulan ini</option>
-                            <option value="yearly">Tahun ini</option>
+                        <x-select wire:model.live="period" name="period" class="text-xs">
+                            <option value="week">Minggu ini</option>
+                            <option value="month">Bulan ini</option>
+                            <option value="year">Tahun ini</option>
                         </x-select>
                     </div>
                 </div>
                 <div class="mt-4 text-center lg:text-start">
                     <p class="font-medium">Transaksi</p>
-                    <p class="text-2xl font-semibold">10</p>
+                    <p class="text-2xl font-semibold">{{ $transactions }}</p>
                 </div>
             </div>
         </div>
@@ -27,7 +27,7 @@
                 <div class="flex items-center justify-between">
                     <div class="space-y-2">
                         <p class="text-sm font-medium text-gray-600">Program</p>
-                        <p class="text-3xl font-semibold tracking-tighter">1</p>
+                        <p class="text-3xl font-semibold tracking-tighter">{{ $programs }}</p>
                     </div>
                     <div>
                         <x-icons.program class="p-2 text-blue-600 w-14 h-14 bg-blue-100/50 rounded-xl" />
@@ -47,7 +47,7 @@
                 <div class="flex items-center justify-between">
                     <div class="space-y-2">
                         <p class="text-sm font-medium text-gray-600">Jenis investasi</p>
-                        <p class="text-3xl font-semibold tracking-tighter">1</p>
+                        <p class="text-3xl font-semibold tracking-tighter">{{ $investments }}</p>
                     </div>
                     <div>
                         <x-icons.investment class="p-2 text-blue-600 w-14 h-14 bg-blue-100/50 rounded-xl" />
@@ -65,7 +65,7 @@
         <div class="flex flex-col border rounded-lg">
             <div class="flex-1 p-4 space-y-2 text-center">
                 <p class="text-sm font-medium text-gray-600">Total Pengguna</p>
-                <p class="text-3xl font-semibold">100</p>
+                <p class="text-3xl font-semibold">{{ $users }}</p>
             </div>
             <x-separator />
             <div class="p-4 text-center">
@@ -95,33 +95,33 @@
                     </tr>
                 </thead>
                 <tbody class="divide-y divide-gray-200">
-                    {{-- @forelse ($transactions as $item) --}}
-                    <tr>
-                        <td class="px-6 py-2 text-sm font-medium text-gray-800 whitespace-nowrap">
-                            {{ Carbon\Carbon::now()->locale('id')->translatedFormat('d F') }}
-                        </td>
-                        <td class="px-6 py-2 text-sm text-gray-800 whitespace-nowrap">
-                            Thoriq Al Hakim</td>
-                        <td class="px-6 py-2 text-sm text-gray-800 whitespace-nowrap">
-                            Program</td>
-                        <td class="px-6 py-2 text-sm text-gray-800 whitespace-nowrap">
-                            <span>Program 1</span>
-                            {{-- @if ($item->transactionable_type == 'App\Models\Program') --}}
-                            - <span class="capitalize">Personal</span>
-                            {{-- @endif --}}
-                        </td>
-                        <td class="px-6 py-2 text-sm text-gray-800 whitespace-nowrap">
-                            Rp30.000.000
-                        </td>
-                    </tr>
-                    {{-- @empty
-                                <tr>
-                                    <td class="px-6 py-4 text-sm font-medium text-center text-gray-800 whitespace-nowrap"
-                                        colspan="6">
-                                        Transaksi tidak ditemukan
-                                    </td>
-                                </tr>
-                            @endforelse --}}
+                    @forelse ($recentTransactions as $item)
+                        <tr>
+                            <td class="px-6 py-2 text-sm font-medium text-gray-800 whitespace-nowrap">
+                                {{ Carbon\Carbon::parse($item->transaction_date)->locale('id')->translatedFormat('d F') }}
+                            </td>
+                            <td class="px-6 py-2 text-sm text-gray-800 whitespace-nowrap">
+                                {{ $item->user->name }}</td>
+                            <td class="px-6 py-2 text-sm text-gray-800 whitespace-nowrap">
+                                {{ class_basename($item->transactionable_type) }}</td>
+                            <td class="px-6 py-2 text-sm text-gray-800 whitespace-nowrap">
+                                <span>{{ $item->transactionable->name }}</span>
+                                @if ($item->transactionable_type == 'App\Models\Program')
+                                    - <span class="capitalize">{{ $item->transaction_type }}</span>
+                                @endif
+                            </td>
+                            <td class="px-6 py-2 text-sm text-gray-800 whitespace-nowrap">
+                                {{ 'Rp' . number_format($item->amount, 0, ',', '.') }}
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td class="px-6 py-4 text-sm font-medium text-center text-gray-800 whitespace-nowrap"
+                                colspan="6">
+                                Transaksi tidak ditemukan
+                            </td>
+                        </tr>
+                    @endforelse
                 </tbody>
             </table>
         </x-table>
