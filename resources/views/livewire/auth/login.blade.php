11211ba0d64f53d704001c41a8d8f5011814c11f
Thoriq Al Hakim <85446784+thoriqalhakim28@users.noreply.github.com>
2025-01-17 14:04:38 +0700

    feat(ui): auth layout

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,40 +1,32 @@
 <div>
-    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div class="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
-            <x-logo class="w-auto h-24 mx-auto" />
-            <h1 class="text-2xl font-semibold leading-7 text-center">Masuk</h1>
-            <div class="mt-6">
-                <form wire:submit.prevent="authenticate">
-                    <div>
-                        <x-label for="email" value="Email" />
-                        <x-input wire:model.lazy="email" id="email" name="email" type="email" required
-                            autofocus />
-                        @error('email')
-                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <div class="mt-4">
-                        <x-label for="password" value="Kata sandi" />
-                        <x-input wire:model.lazy="password" id="password" name="password" type="password" required
-                            autofocus />
-                        @error('password')
-                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <div class="flex justify-end mt-4">
-                        <a href="{{ route('password.request') }}"
-                            class="text-sm font-medium text-indigo-600 transition duration-150 ease-in-out hover:text-indigo-500 focus:outline-none focus:underline">
-                            Lupa kata sandi?
-                        </a>
-                    </div>
-                    <div class="mt-6">
-                        <x-button class="w-full">
-                            Masuk
-                        </x-button>
-                    </div>
-                </form>
+    <div class="mb-8 text-center">
+        <h1 class="text-4xl font-semibold font-display">
+            Selamat Datang
+        </h1>
+        <p>Masukan email dan kata sandi untuk mengakses akun anda.</p>
+    </div>
+    <div class="mt-6">
+        <form wire:submit.prevent="authenticate">
+            <div>
+                <x-label for="email" value="Email" />
+                <x-input wire:model.lazy="email" id="email" name="email" type="email" required autofocus />
+                @error('email')
+                    <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                @enderror
             </div>
-        </div>
+            <div class="mt-4">
+                <x-label for="password" value="Kata sandi" />
+                <x-input wire:model.lazy="password" id="password" name="password" type="password" required autofocus />
+                @error('password')
+                    <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="mt-6">
+                <x-button class="w-full">
+                    Masuk
+                </x-button>
+            </div>
+        </form>
     </div>
 </div>
 
